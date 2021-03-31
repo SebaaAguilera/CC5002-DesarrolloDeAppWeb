46ccc57194f5f6e234def56ed7617aa4a30d70f5
SebaaAguilera <saguilerav98@gmail.com>
2021-03-31 19:39:07 -0300

    Front base del form

--- assets/js/lastReports.js
+++ assets/js/lastReports.js
@@ -1,18 +1,8 @@
 // Images taken from https://www.insectimages.org/index.cfm
 
-let dummy;
-
-fetch('./assets/json/dummy.json')
-.then(response => {
-    console.log(response);
-    return response.json();
-})
-.then(data => {
-    dummy = data.avistamientos.slice(0,5);
-    console.log('DUMMY', dummy);
-
+const parseHTML = (data) => {
     let html = "";
-    dummy.forEach(element => {
+    data.forEach(element => {
         html = html.concat(`
             <div class="home-report">
                 <div class="description">
@@ -26,10 +16,47 @@ fetch('./assets/json/dummy.json')
                 </div>
             </div>`);
     });
+    return html
+};
 
+/* **** MAIN **** */
+
+fetch('./assets/json/dummy.json').then(response => {
+    console.log(response);
+    return response.json();
+}).then(data => {
+    const dummy = data.avistamientos.slice(0,5);
+    console.log('DUMMY', dummy);
 
     let lastReports = document.getElementById('lastReports');
-    lastReports.innerHTML = html;
+    lastReports.innerHTML = parseHTML(dummy);
+}).catch(e => {
+    console.log('Si se abre el index.html a secas en el navegador no funciona el fetch :c')
+    console.error(e)
 
+    const dummy = [
+        {
+           "fecha": "2021-03-29 13:21",
+           "comuna": "La Florida",
+           "sector": "Vicuña Mackena",
+           "tipo": "insecto",
+           "img": {
+               "src":"https://bugwoodcloud.org/images/384x256/5386131.jpg",
+               "alt":"chinita"
+           }
+        },
+        {
+            "fecha": "2021-03-27 10:03",
+            "comuna": "Providencia",
+            "sector": "Plaza Italia",
+            "tipo": "arácnido",
+            "img": {
+                "src":"https://bugwoodcloud.org/images/384x256/5506414.jpg",
+                "alt":"tarantula"
+            }
+         }
+    ];
 
-})
+    let lastReports = document.getElementById('lastReports');
+    lastReports.innerHTML = parseHTML(dummy);
+});
