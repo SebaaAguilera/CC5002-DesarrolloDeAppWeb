8dc2e99b5aee3a2f9f81caee46625ddde293c1bc
SebaaAguilera <saguilerav98@gmail.com>
2021-04-03 23:20:06 -0400

    Se agregan archivos donde debe

--- assets/js/form_new_report.js
+++ assets/js/form_new_report.js
@@ -4,10 +4,16 @@ function newReport() {
     const newReport = reports[reports.length-1].cloneNode(true);
     newReport.setAttribute("id", `report-${reports.length}`);
     newReport.children[0].children[0].innerHTML = `Información de avistamiento n°${reports.length+1}`;
-    console.log(newReport.children[4].children[1].children[0].children[1])
-    newReport.children[4].children[1].id = `file-input-list-${reports.length}`
-    newReport.children[4].children[1].children[0].children[1].id = `button-avistamiento-${reports.length}`
-    console.log(newReport.children[4].children[1].children[0].children[1])
 
+    const fileInputList = newReport.children[4].children[1];
+
+    fileInputList.id = `file-input-list-${reports.length}`;
+    fileInputList.children[0].children[0].children[1].id = `foto-avistamiento-${reports.length}-0`;
+    fileInputList.children[0].children[1].id = `button-avistamiento-${reports.length}`;
+
+    while (fileInputList.children.length > 1) {
+        fileInputList.removeChild(fileInputList.children[fileInputList.children.length-1]);
+    };
+    
     reportsDiv.appendChild(newReport);
 }
