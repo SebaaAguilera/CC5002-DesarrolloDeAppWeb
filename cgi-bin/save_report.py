ddcac555851d1680257d4f15af04b0ac42ec7419
SebaaAguilera <saguilerav98@gmail.com>
2021-05-07 15:01:51 -0400

    Pseudoformateo de data y manejo de archivos

--- cgi-bin/save_report.py
+++ cgi-bin/save_report.py
@@ -7,9 +7,12 @@ cgitb.enable()
 import db
 import json
 import os
-import datetime
-import hashlib
+import datetime as dt
+import hashlib as hl
 import filetype
+
+MEDIAPATH = 'media/'
+MEDIAERROR = False
 
 reportDB = db.ReportDB()
 form = cgi.FieldStorage()
@@ -25,6 +28,25 @@ def cantidadAvistamientos():
             error = True
     return cant
 
+def save_media(field):
+    datetime = str(dt.datetime.now())
+    filename = datetime + os.path.basename(field.filename)
+    filename = hl.sha256(filename.encode()).hexdigest()[0:30]
+    file_path = MEDIAPATH + filename
+    open(file_path, 'wb').write(field.file.read())
+
+    tipo = filetype.guess(file_path)
+    if tipo.mime[0:5] != 'image':
+        os.remove(file_path)
+        raise Exception('Archivo no es una imagen')
+    else:
+        return filename
+
+def delete_media(reports):
+    for report in reports:
+        for foto in fotos:
+            os.remove(MEDIAPATH + foto)
+
 print("Content-type:text/html\r\n\r\n")
 print("""
 <!DOCTYPE html>
@@ -39,47 +61,77 @@ print("<button onclick='window.history.back();'>Go Back</button>")
 # Lugar
 fieldComuna = form['comuna']
 fieldSector = form['sector']
-print('<b>Lugar</b><br>')
-print(fieldComuna,'<br>')
-print(fieldSector,'<br>')
+
 # Contacto
 fieldNombre = form['nombre']
 fieldEmail = form['email']
 fieldFono = form ['celular']
-print('<b>Contacto</b><br>')
-print(fieldNombre,'<br>')
-print(fieldEmail,'<br>')
-print(fieldFono,'<br>')
 
 # Avistamientos
-print('<b>Avistamientos</b><br>')
-
 index = 0
 reports = []
-while index < cantidadAvistamientos():
+for index in range(0,cantidadAvistamientos()):
     fieldDiaHora = form [f'dia-hora-avistamiento-{index}']
     fieldTipo = form [f'tipo-avistamiento-{index}']
     fieldEstado = form [f'estado-avistamiento-{index}']
     fieldFoto = form [f'foto-avistamiento-{index}']
 
+    fotos = []
+    # Que horrendoo esto
     if isinstance(fieldFoto, list):
         for foto in fieldFoto:
-            print(index, foto,'<br>')
+            if foto.filename:
+                try:
+                    filename = save_media(foto)
+                    fotos.append(filename)
+                except:
+                    MEDIAERROR =  True
     else:
-        print(index, fieldFoto,'<br>')
-    index += 1
+        if fieldFoto.filename:
+            try:
+                filename = save_media(fieldFoto)
+                fotos.append(filename)
+            except:
+                MEDIAERROR =  True
 
+    if MEDIAERROR:
+        delete_media(reports)
+        break
 
+    reports.append({
+        'dia_hora': fieldDiaHora.value,
+        'tipo': fieldTipo.value,
+        'estado': fieldEstado.value,
+        'fotos': fotos
+    })
 
-# reportDB.save_data({
-#     'ruta_media': 'media/',
-#     'comuna_id': fieldComuna.value,
-#     'sector': fieldSector.value,
-#     'nombre': fieldNombre.value,
-#     'email': fieldEmail.value,
-#     'celular': fieldFono.value,
-#     'reports': []
-# })
+if MEDIAERROR:
+    print('<h3>Fotos no cumplen con el formato</h3>')
+else:
+    try:
+        print('<h3>Se van a guardar las cosas</h3>')
+        print({
+            'ruta_media': 'media/',
+            'comuna_id': fieldComuna.value,
+            'sector': fieldSector.value,
+            'nombre': fieldNombre.value,
+            'email': fieldEmail.value,
+            'celular': fieldFono.value,
+            'reports': reports
+        })
+        # reportDB.save_data({
+        #     'ruta_media': 'media/',
+        #     'comuna_id': fieldComuna.value,
+        #     'sector': fieldSector.value,
+        #     'nombre': fieldNombre.value,
+        #     'email': fieldEmail.value,
+        #     'celular': fieldFono.value,
+        #     'reports': reports
+        # })
+    except:
+        delete_media(reports)
+        print('<h3>Oh no!</h3>')
+
 
 print("""
 </body>
